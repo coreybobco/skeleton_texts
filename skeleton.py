dc773d7957f8a007249b349b60f70db94757c3aa
Corey Bobco <corey.bobco@gmail.com>
2015-11-02 20:58:56 -0600

    skeleton fixes

--- skeleton.py
+++ skeleton.py
@@ -10,34 +10,42 @@ class Skeleton_Class:
 		self.skeleton = skeleton
 
 	def generate(self):
-		frequency_scale_bones = list(re.findall("\{frequency_scale\([ABCDEF],[\s]*[\S]*\)}", self.skeleton))
-		frequency_bone_split = list(re.split("{\{frequency_scale\([ABCDEF],[\s]*[\S]*\)}", self.skeleton))
+		frequency_scale_regex = "\{frequency_scale\([ABCDEF],[\s]*[\S]*\)}"
+		frequency_scale_bones = re.findall(frequency_scale_regex, self.skeleton)
+		frequency_bone_split = re.split(frequency_scale_regex, self.skeleton)
 		frequency_scale_flesh = []
-		print(frequency_scale_bones)
+		print(len(frequency_scale_bones))
 		for bone in frequency_scale_bones:
 			t_index = self.get_text_index(bone)
 			frequency_scale = int(re.search("[\d]+", self.skeleton).group(0))
 			frequency_scale_flesh.append(self.texts[t_index].random_word_by_freq_scale(frequency_scale))
 		flesh_text = ""
+		print("Frequency Scales:")
+		print(frequency_bone_split)
+		print(len(frequency_bone_split))
 		for index, static_bone in enumerate(frequency_bone_split):
-			if index == len(frequency_bone_split):
+			if index == len(frequency_bone_split) - 1:
 				flesh_text += static_bone
 			else:
 				flesh_text += static_bone + frequency_scale_flesh[index]
 
-		old_flesh = flesh_text
-		portmanteau_bones = list(re.findall("{portmanteau\([ABCDEF],[\s]*[\S]*\)}", old_flesh))
-		portmanteau_bone_split = list(re.split("{portmanteau\([ABCDEF],[\s]*[\S]*\)}", old_flesh))
+		print(flesh_text)
+		portmanteau_regex = "{portmanteau\([ABCDEF],[\s]*[\S]*\)}"
+		portmanteau_bones = re.findall(portmanteau_regex, flesh_text)
+		portmanteau_bone_split = re.split(portmanteau_regex, flesh_text))
+		print("Portmanteaus")
 		portmanteau_flesh = []
 		for bone in portmanteau_bones:
 			t_index = self.get_text_index(bone)
 			portmanteau_flesh.append(self.texts[t_index].naive_random_portmanteau())
+		print(portmanteau_flesh)
+		print(portmanteau_bone_split)
 		for index, bone in enumerate(portmanteau_bone_split):
-			if index == len(portmanteau_bone_split):
+			if index == len(portmanteau_bone_split) - 1:
 				flesh_text += static_bone
 			else:
 				flesh_text += static_bone + portmanteau_flesh[index]
-		return flesh
+		return flesh_text
 
 	def get_text_index(self, bone):
 		text_alphakey = re.search("\([ABCDEF],", bone).group(0)
